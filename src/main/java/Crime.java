76f4827b54a3358661058f48f3e762a8c8959e46
wbeeker <152426086+wbeeker@users.noreply.github.com>
2024-07-24 08:02:10 -0700

    Added javadocs and removed location_subtype

--- src/main/java/Crime.java
+++ src/main/java/Crime.java
@@ -1,23 +1,34 @@
 import java.util.Objects;
 
+/**
+ * This class holds the crime objects built from the details of the crime report.
+ */
 public class Crime {
+    /** The type of crime committed. */
     private String category;
-    private String locationType;
+    /** The latitude at which the crime occurred. */
     private String locationLatitude;
+    /** The ID of the street where the crime occurred. */
     private int streetID;
+    /** The name of the street where the crime occurred (often an approximate location: i.e. near Bridge street). */
     private String streetName;
+    /** The longitude where the crime occurred. */
     private String locationLongitude;
+    /** The outcome_status category (i.e. investigation completed). */
     private String outcomeCategory;
+    /** The date of the outcome_status. */
     private String outcomeDate;
+    /** The persistent ID string (mix of letters and numbers). */
     private String persistentID;
+    /** The ID number for the crime report. */
     private int ID;
+    /** The month the crime occurred. */
     private String month;
 
-    public Crime (String category, String locationType, String locationLatitude, int streetID, String streetName,
+    public Crime (String category, String locationLatitude, int streetID, String streetName,
                   String locationLongitude, String outcomeCategory, String outcomeDate, String persistentID, int ID,
                   String month) {
         this.category = category;
-        this.locationType = locationType;
         this.locationLatitude = locationLatitude;
         this.streetID = streetID;
         this.streetName = streetName;
@@ -29,49 +40,89 @@ public class Crime {
         this.month = month;
     }
 
+    /**
+     * Gets the category of the crime committed.
+     * @return category
+     */
     public String getCategory() {
         return category;
     }
 
-    public String getLocationType() {
-        return locationType;
-    }
-
+    /**
+     * Gets the latitude where the crime occurred.
+     * @return location latitude
+     */
     public String getLocationLatitude() {
         return locationLatitude;
     }
 
+    /**
+     * Gets the street ID number.
+     * @return street ID number
+     */
     public int getStreetID() { return streetID; }
 
+    /**
+     * Gets the street name (or approximate location).
+     * @return street name
+     */
     public String getStreetName() {
         return streetName;
     }
 
+    /**
+     * Gets the longitude where the crime occurred.
+     * @return location longitude
+     */
     public String getLocationLongitude() {
         return locationLongitude;
     }
 
+    /**
+     * Gets the outcome_status category.
+     * @return outcome category
+     */
     public String getOutcomeCategory() {
         return outcomeCategory;
     }
 
+    /**
+     * Gets the date of the outcome_status.
+     * @return outcome date
+     */
     public String getOutcomeDate() {
         return outcomeDate;
     }
 
+    /**
+     * Gets the persistent ID for the crime report.
+     * @return persistent ID
+     */
     public String getPersistentID() {
         return persistentID;
     }
 
+    /**
+     * Gets the ID number for the crime report.
+     * @return ID number
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     * Gets the month the crime was committed.
+     * @return month
+     */
     public String getMonth() {
         return month;
     }
 
-
+    /**
+     * Compares Crime objects to see if they're equal.
+     * @param o crime object
+     * @return true or false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -80,7 +131,6 @@ public class Crime {
         return streetID == crime.streetID
                 && ID == crime.ID
                 && Objects.equals(category, crime.category)
-                && Objects.equals(locationType, crime.locationType)
                 && Objects.equals(locationLatitude, crime.locationLatitude)
                 && Objects.equals(streetName, crime.streetName)
                 && Objects.equals(locationLongitude, crime.locationLongitude)
@@ -90,9 +140,13 @@ public class Crime {
                 && Objects.equals(month, crime.month);
     }
 
+    /**
+     * Gets a unique hashcode for a crime object.
+     * @return hashcode integer
+     */
     @Override
     public int hashCode() {
-        return Objects.hash(category, locationType, locationLatitude, streetID, streetName, locationLongitude,
+        return Objects.hash(category, locationLatitude, streetID, streetName, locationLongitude,
                 outcomeCategory, outcomeDate, persistentID, ID, month);
     }
 }
