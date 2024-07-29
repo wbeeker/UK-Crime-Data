719ec93bb5aba66ba5141f11cbab2518222dd473
zembug <150472880+zembug@users.noreply.github.com>
2024-07-29 15:07:14 -0400

    updates

--- src/main/java/CrimeGUI.java
+++ src/main/java/CrimeGUI.java
@@ -1,19 +1,25 @@
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+
 
 
 public class CrimeGUI extends JFrame {
 
     private JPanel panel;
     private JTextField crimeTextField;
+    private JComboBox categories;
     private JButton crimeButton;
     private JButton detailsButton;
     private JButton mapButton;
@@ -22,14 +28,18 @@ public class CrimeGUI extends JFrame {
     private JButton locationButton;
 
     private JPanel topPanel;
-    private JPanel crimePanel;
     private JPanel buttonPanel;
+    private JPanel mapPanel;
 
-    private JTextArea textArea;
+    private JTextArea info;
+    private JTextArea stats;
+    private JScrollPane scrollPane;
 
-
+    private String[]categoryList = {"all"};
+    //private CrimeList crimeList;
 
     public CrimeGUI() {
+        //crimeList = new CrimeList();
         setTitle("Crime: UK Police Data GUI");
         setSize(800, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,12 +49,18 @@ public class CrimeGUI extends JFrame {
         panel.setLayout(new BorderLayout());
 
 
-        crimePanel = new JPanel();
         buttonPanel = new JPanel();
         topPanel = new JPanel();
-        textArea = new JTextArea();
+        mapPanel = new JPanel();
 
 
+        info = new JTextArea("Crime Info");
+        stats = new JTextArea("Crime Stats");
+        scrollPane = new JScrollPane(info);
+        scrollPane.setPreferredSize(new Dimension(200, 700));
+        stats.setPreferredSize(new Dimension(200, 700));
+
+        categories = new JComboBox(categoryList);
         crimeTextField = new JTextField(10);
         crimeButton = new JButton("Crime Search");
         crimeButton.addActionListener(new crimeSearchListener());
@@ -59,26 +75,20 @@ public class CrimeGUI extends JFrame {
         timelineButton = new JButton("Timeline");
         timelineButton.addActionListener(new timelineListener());
 
+        topPanel.add(categories);
+        topPanel.add(crimeButton);
 
-
-
-        crimePanel.add(crimeTextField);
-        crimePanel.add(crimeButton);
         buttonPanel.add(detailsButton);
         buttonPanel.add(mapButton);
         buttonPanel.add(statsButton);
         buttonPanel.add(timelineButton);
         buttonPanel.add(locationButton);
 
-
-        topPanel.add(crimePanel);
-
-
         panel.add(topPanel, BorderLayout.NORTH);
         panel.add(buttonPanel, BorderLayout.SOUTH);
-        panel.add(textArea, BorderLayout.CENTER);
-
-
+        panel.add(scrollPane, BorderLayout.WEST);
+        panel.add(stats, BorderLayout.EAST);
+        panel.add(mapPanel, BorderLayout.CENTER);
         add(panel);
 
         setVisible(true);
@@ -92,44 +102,51 @@ public class CrimeGUI extends JFrame {
     private class detailsListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            textArea.setText("  Details");
+            //textArea.setText("  Details");
         }
     }
 
     private class crimeSearchListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String crime = crimeTextField.getText();
-            textArea.setText("  " + crime);
+            //String crime = crimeTextField.getText();
+            //textArea.setText("  " + crime);
+            //ArrayList<Crime> crimes = crimeList.getCrimes();
+            String output = "";
+            //for (Crime crime: crimes) {
+                //output += crime.toString();
+            }
+            //info.setText(output);
         }
-    }
+    
 
     private class mapListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            textArea.setText("  Map");
+            //textArea.setText("  Map");
         }
     }
 
     private class statsListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            textArea.setText("  Stats");
+            //textArea.setText("  Stats");
         }
     }
 
     private class timelineListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            textArea.setText("  Timeline");
+            //textArea.setText("  Timeline");
         }
     }
 
     private class locationListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            textArea.setText("  Location Details");
+            //textArea.setText("  Location Details");
         }
     }
 
 }
+
